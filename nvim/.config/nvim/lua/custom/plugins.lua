2104baaf0622f2ede29e0320ec98b56d25f67afe
Sam Gabel <sgabel331@gmail.com>
2023-04-11 05:26:16 -0700

    adding only custom nvim files

---
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,17 @@
+local plugins = {
+  {
+    "vim-crystal/vim-crystal",
+    ft = "crystal",
+    config = function(_)
+      vim.g.crystal_auto_format = 1
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+return plugins
