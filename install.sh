d0a8687d99aa394196d99d2b42e913ce234c7da3
Sam Gabel <sgabel331@gmail.com>
2023-04-11 22:27:32 -0700

    fixed install.sh & minor tweak to lspconfig

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # --------Install homebrew, packages, and stow (create symlinks)---------
 # ONLY RUN IF ON MACOS
 
 # Define the packages to be installed
-declare -a packages=("exa" "stow" "starship" "zsh-autosuggestions" "zsh-syntax-highlighting" "neofetch")
+declare -a packages=("exa" "stow" "starship" "zsh-autosuggestions" "zsh-syntax-highlighting" "neofetch" "neovim" "python3" "node")
 
 # check if Homebrew is installed
 if ! command -v brew >/dev/null; then
@@ -35,27 +37,42 @@ done
 
 echo "################################################"
 
+#NvChad Installation & remove custom default files
+echo "B-R-U-H (NvChad)"
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+rm -rf ~/.config/nvim/lua/custom/
+
+echo "################################################"
+
 # stow
-echo "\nStowing ZSH Configurations..."
+echo "Stowing ZSH Configurations..."
 stow zsh
-echo "Stowing VIM Configurations..."
-stow vim
+echo "Stowing neoVIM Configurations..."
+stow nvim
 
 echo "################################################"
 
 # Check for a compatible Nerd Font
 if ! command -v gf >/dev/null; then
-  echo "\nNerd Font is not installed."
-  echo "\nList of Nerd Fonts: \nhttps://www.nerdfonts.com/font-downloads"
-  echo "Brew Install instructions: \nhttps://gist.github.com/davidteren/898f2dcccd42d9f8680ec69a3a5d350e"
+  echo "A Nerd Font is required for this to work"
+  echo "List of Nerd Fonts: https://www.nerdfonts.com/font-downloads"
+  echo "Brew Install instructions: https://gist.github.com/davidteren/898f2dcccd42d9f8680ec69a3a5d350e"
 else
-  echo "\nNerd Font is installed."
+  echo "Nerd Font is installed."
 fi
+
+echo "################################################"
 
 # Set ZSH as the default shell
 if [ "$SHELL" != "/bin/zsh" ]; then
   echo "Setting ZSH as default shell..."
-  chsh -s $(which zsh)
+  chsh -s "$(which zsh)"
+  echo "Please retart terminal session to apply chsh settings"
 else
   echo "ZSH is already set as the default shell!."
 fi
