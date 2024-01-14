c0df70ddd78f8c18f990dbc4ea05a14ba0937856
Sam Gabel <sam@gabelcloud.com>
2024-01-14 13:55:41 -0800

    additional server-side aliases

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -3,6 +3,9 @@ alias flushhostdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponde
 
 # server shortcuts
 alias dps="docker ps --format 'table {{.Names}}\t {{.ID}}\t {{.Image}}\t {{.Status}}\t'"
+alias dcup="docker compose up -d"
+alias dcupr="docker compose up -d --force-recreate"
+alias dcl="docker compose logs -f"
 
 # ALIAS COMMANDS
 alias ls="eza --icons --group-directories-first"
