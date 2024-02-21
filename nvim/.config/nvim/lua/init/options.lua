42f64fc7b3cb4621b6911af6bd9add499ee3177f
Sam Gabel <sam@gabelcloud.com>
2024-02-21 00:39:06 -0800

    added nvim options and custom lualine-tmux element

--- nvim/.config/nvim/lua/init/options.lua
+++ nvim/.config/nvim/lua/init/options.lua
@@ -4,7 +4,8 @@ vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying me
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
 -- vim.opt.fileencoding = "utf-8" -- the encoding written to a file
-vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.hlsearch = false -- highlight all matches on previous search pattern
+vim.opt.incsearch = true
 vim.opt.ignorecase = true -- ignore case in search patterns
 vim.opt.mouse = "a" -- allow the mouse to be used in neovim
 vim.opt.pumheight = 10 -- pop up menu height
@@ -19,7 +20,7 @@ vim.opt.swapfile = false -- creates a swapfile
 vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true -- enable persistent undo
-vim.opt.updatetime = 100 -- faster completion (4000ms default)
+vim.opt.updatetime = 50 -- faster completion (4000ms default)
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
@@ -33,7 +34,7 @@ vim.opt.relativenumber = true -- set relative numbered lines
 vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false -- display lines as one long line
-vim.opt.scrolloff = 0
+vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 vim.opt.title = false
@@ -51,8 +52,13 @@ vim.opt.conceallevel = 1 -- conceals things like code block (```), bold (**), an
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 
+-- TODO: figure out how to replace netrw
 vim.g.netrw = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_mouse = 2
 
-vim.cmd "se nohlsearch"
+vim.opt.breakindent = true -- respects indentation when soft wrapping lines
+vim.opt.breakindentopt = "shift:2"
+
+vim.api.nvim_command([[autocmd VimEnter * silent !tmux set status off]])
+vim.api.nvim_command([[autocmd VimLeave * silent !tmux set status on]])
--- nvim/.config/nvim/lua/plugin/lualine.lua
+++ nvim/.config/nvim/lua/plugin/lualine.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: undefined-field, need-check-nil
+
 local M = {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
@@ -7,7 +9,34 @@ local M = {
   },
 }
 
+
+-- Global variables to store tmux session name and zoom state
+local tmux_session_name = ""
+local tmux_zoomed = false
+-- Function to update tmux session name and zoom state
+local function update_tmux_session_info()
+    local handle = io.popen("tmux display-message -p '#S'")
+    tmux_session_name = handle:read("*l") or ""
+    handle:close()
+    -- lists whether the pane is zoomed or not
+    handle = io.popen("tmux list-panes -F '#F'")
+    local zoom_info = handle:read("*a") or ""
+    handle:close()
+    -- if there is a "Z" in the list then the pane is zoomed
+    tmux_zoomed = zoom_info:find("Z") ~= nil
+end
+-- Initial update of tmux session info
+update_tmux_session_info()
+-- Get tmux session name and check if the current pane is zoomed
+local function get_tmux_session()
+    return tmux_session_name .. (tmux_zoomed and " (󰍉)" or "")
+end
+-- Periodically update tmux session info (every 10 seconds) avoid stuttering when scrolling
+vim.loop.new_timer():start(5000, 5000, vim.schedule_wrap(update_tmux_session_info))
+
+
 function M.config()
+
   require("lualine").setup {
     options = {
       component_separators = { left = "", right = "" },
@@ -17,16 +46,20 @@ function M.config()
     sections = {
       lualine_a = { },
       lualine_b = {
+        {
+          require("noice").api.status.mode.get,
+          cond = require("noice").api.status.mode.has,
+          color = { fg = "#ff9e64" },
+        },
         require("nomodoro").status,
       },
       lualine_c = { "branch", "diff" },
       lualine_x = { "diagnostics", "copilot", "filetype" },
       lualine_y = { "progress" },
-      lualine_z = { },
+      lualine_z = { get_tmux_session },
     },
     extensions = { "quickfix", "man", "fugitive" },
   }
 end
 
 return M
-
