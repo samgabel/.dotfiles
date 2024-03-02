ffe3fa7bfb54ccfb4e3536ba900c278fa806b54e
Sam Gabel <sam@gabelcloud.com>
2024-03-01 23:08:51 -0800

    feat(nvim-package): add new plugin silicon

--- nvim/.config/nvim/lua/init/options.lua
+++ nvim/.config/nvim/lua/init/options.lua
@@ -2,7 +2,8 @@ vim.opt.backup = false -- creates a backup file
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
-vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.conceallevel = 1 -- conceals things like code block (```), bold (**), and full path links in .md notes
+vim.opt.concealcursor = "nc" -- works with conceallevel
 -- vim.opt.fileencoding = "utf-8" -- the encoding written to a file
 vim.opt.hlsearch = false -- highlight all matches on previous search pattern
 vim.opt.incsearch = true -- smarter search we can do things like: " vim.* = "
@@ -25,21 +26,20 @@ vim.opt.writebackup = false -- if a file is being edited by another program (or 
 -- INDENTATION
 vim.opt.expandtab = true -- convert tabs to spaces
 vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- insert 2 spaces for a tab
+vim.opt.tabstop = 4 -- insert 4 spaces for a tab
 vim.cmd("set invlist") -- sets the tab literal character visibility on
 vim.cmd("set listchars=tab:║->,trail:") -- formats the trailing dot when typing and the literal tab character
--- vim.opt.listchars
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.number = true -- set numbered lines
-vim.opt.laststatus = 3
-vim.opt.showcmd = false
-vim.opt.ruler = false
+vim.opt.laststatus = 3 -- set lualine behavior
+vim.opt.showcmd = false -- we are using mini.nvim (from noice.nvim)
+vim.opt.ruler = false -- disable line and column number in status line
 vim.opt.relativenumber = true -- set relative numbered lines
-vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
+vim.opt.numberwidth = 2 -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false -- display lines as one long line
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
+vim.opt.scrolloff = 8 -- num of lines to keep above and below cursor when moving
+vim.opt.sidescrolloff = 8 -- num of lines to keep when moving side to side
 vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
 vim.opt.title = false
 -- colorcolumn = "80",
@@ -50,7 +50,6 @@ vim.opt.fillchars:append {
 }
 
 vim.opt.shortmess:append "c" -- cleaner UI for quickfix lists
-vim.opt.conceallevel = 1 -- conceals things like code block (```), bold (**), and full path links in .md notes
 
 vim.opt.whichwrap:append "h,l" -- use vim motions in Normal to go to next line when at the end of the current line and vice-versa
 vim.opt.iskeyword:append "-" -- selects a word with "-" in-between as a full word, before it would treat the word as 2 words
