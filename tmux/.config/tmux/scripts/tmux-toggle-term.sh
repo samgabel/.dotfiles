7f1d2618af9518d5862838921fa67822d8bbeae1
Sam Gabel <sam@gabelcloud.com>
2024-07-16 09:30:12 -0700

    add: tmux script to toggle a split terminal (vscode-like)
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

---
+++ tmux/.config/tmux/scripts/tmux-toggle-term.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $(tmux list-panes | wc -l) -eq 1 ]; then
+    # If there is only one pane, create a horizontal split and focus on the bottom pane
+    tmux send-keys C-s \; split-window -v -l 12
+else
+    # If there are multiple panes, check the zoom state of the top pane
+    if [ $(tmux display-message -p "#{window_zoomed_flag}") -eq 1 ]; then
+        # If the top pane is zoomed, unzoom it
+        tmux send-keys C-s \; resize-pane -Z \; select-pane -t 2
+    else
+        # If the top pane is not zoomed, focus on the top pane and zoom it
+        tmux select-pane -t 1 \; send-keys C-s \; resize-pane -Z
+    fi
+fi
+
