b86bc0e5894922866a11749dcc43c5773bad7b91
Sam Gabel <sam@gabelcloud.com>
2025-05-24 13:06:23 -0700

    chore(nvim): remove lazy loading for noice

--- configs/nvim/.config/nvim/lua/plugin/core/noice.lua
+++ configs/nvim/.config/nvim/lua/plugin/core/noice.lua
@@ -1,6 +1,5 @@
 local M = {
     "folke/noice.nvim",
-    event = "VeryLazy",
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
