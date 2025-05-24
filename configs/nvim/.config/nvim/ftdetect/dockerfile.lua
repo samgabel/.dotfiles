b3fd0ae3e51106c5c94050095e7173a7ea944561
Sam Gabel <sam@gabelcloud.com>
2025-05-24 12:27:16 -0700

    chore(nvim): ftdetect update

--- configs/nvim/.config/nvim/ftdetect/dockerfile.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    group = vim.api.nvim_create_augroup('detect_dockerfile', { clear = true }),
-    desc = 'Set filetype for dockerfile files',
-    pattern = { "[dD]ockerfile.*" },
-    callback = function()
-        vim.bo.filetype = 'dockerfile'
-    end,
-})
-
